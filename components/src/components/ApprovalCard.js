722ea9043c816e05615ad660c74d98689ff340c1
Uri Seadia <uri_s@radiflow.com>
2020-10-08 11:25:54 +0300

    components project

---
+++ components/src/components/ApprovalCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ApprovalCard = ({ children, approveText, rejectText }) => {
+  return (
+    <div className="ui card">
+      <div className="content">{children}</div>
+      <div className="extra content">
+        <div className="ui two buttons">
+          <div className="ui basic green button">{approveText}</div>
+          <div className="ui basic red button">{rejectText}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ApprovalCard;
